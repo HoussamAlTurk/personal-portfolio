2ce001ec62b2897d776e84f3126c081d912a5909
Houssam <houss2004@gmail.com>
2026-02-09 22:43:23 +0400

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,27 +6,25 @@ import { ComputersCanvas } from "./canvas";
 const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
-      <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-      >
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
 
         <div>
-          <h1 className={`${styles.heroHeadText} text-white`}>
+          <h1 className={`text-white font-black text-[36px] sm:text-[48px] md:text-[60px] lg:text-[72px]`}>
             Hi, I'm <span className='text-[#915EFF]'>Houssam</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            Computer Science Student @ AUS
+            Computer Science Student @ AUS <br className="sm:block hidden" />
           </p>
         </div>
       </div>
       
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-0 bottom-0 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
